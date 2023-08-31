47b16b453ef56d1174b1bc25dcc44d38497779b0
Abdullah M. Bakry <abdaullah62@gmail.com>
2023-08-31 17:48:53 +0300

    fix conversion breaking duo to an error

--- converter/index.ts
+++ converter/index.ts
@@ -23,62 +23,75 @@ const bucket = process.env.AWS_S3_BUCKET_NAME!
 
 //! Convert function: all the work will be done here
 const convert = async (c: Conversion) => {
-    const s3 = new AWS.S3()
-    const downloadParams = {
-        Bucket: bucket,
-        Key: c.s3Key,
-    }
-    console.log('Downloading file:', downloadParams)
-    const res = await s3.getObject(downloadParams).promise()
+    try {
+        const s3 = new AWS.S3()
+        const downloadParams = {
+            Bucket: bucket,
+            Key: c.s3Key,
+        }
+        console.log('Downloading file:', downloadParams)
+        const res = await s3.getObject(downloadParams).promise()
 
-    const converters = findPath(c.fromMime, c.toMime)
+        const converters = findPath(c.fromMime, c.toMime)
 
-    if (!converters) {
-        console.error(
-            `Could not find a converters for ${c.fromMime} to ${c.toMime}`
-        )
+        if (!converters) {
+            console.error(
+                `Could not find a converters for ${c.fromMime} to ${c.toMime}`
+            )
+            await prisma.conversion.update({
+                where: {
+                    id: c.id,
+                },
+                data: {
+                    error: `Could not convert from ${c.fromMime} to ${c.toMime}`,
+                    status: ConversionStatus.ERROR,
+                },
+            })
+            return
+        }
+
+        // otherwise, we will have a path of converters and we need to loop over them
+        let converted = res.Body as Buffer // this is the file we will convert
+        for (const edge of converters) {
+            converted = await edge.converter(res.Body as Buffer) // convert the file
+        }
+
+        console.log('Convert', converters[converters.length - 1].to.type)
+        const mime = extension(
+            converters[converters.length - 1].to.type
+        ) as string // get the mime type of the last converter as it will be the required mime type to
+
+        const key = (randomUUID() + randomUUID()).replace(/-/g, '') //  create a new random key for the file after it has been converted
+        console.log('Uploading to:', key)
+        const uploadParams = {
+            Bucket: bucket,
+            Key: key,
+            Body: converted,
+        }
+        await s3.putObject(uploadParams).promise()
+
         await prisma.conversion.update({
             where: {
                 id: c.id,
             },
             data: {
-                error: `Could not convert from ${c.fromMime} to ${c.toMime}`,
-                status: ConversionStatus.ERROR,
+                status: ConversionStatus.DONE,
+                s3Key: key,
+                currentMime: mime,
             },
         })
-        return
+        //! RECAP: we have downloaded the file from s3, converted it, and uploaded it back to s3, and then update the database
+    } catch (error: any) {
+        await prisma.conversion.update({
+            where: {
+                id: c.id,
+            },
+            data: {
+                status: ConversionStatus.ERROR,
+                error: `Could not convert: ${error.message}`,
+            },
+        })
     }
-
-    // otherwise, we will have a path of converters and we need to loop over them
-    let converted = res.Body as Buffer // this is the file we will convert
-    for (const edge of converters) {
-        converted = await edge.converter(res.Body as Buffer) // convert the file
-    }
-
-    console.log('Convert', converters[converters.length - 1].to.type)
-    const mime = extension(converters[converters.length - 1].to.type) as string // get the mime type of the last converter as it will be the required mime type to
-
-    const key = (randomUUID() + randomUUID()).replace(/-/g, '') //  create a new random key for the file after it has been converted
-    console.log('Uploading to:', key)
-    const uploadParams = {
-        Bucket: bucket,
-        Key: key,
-        Body: converted,
-    }
-    await s3.putObject(uploadParams).promise()
-
-    await prisma.conversion.update({
-        where: {
-            id: c.id,
-        },
-        data: {
-            status: ConversionStatus.DONE,
-            s3Key: key,
-            currentMime: mime,
-        },
-    })
-
-    //! RECAP: we have downloaded the file from s3, converted it, and uploaded it back to s3, and then update the database
 }
 
 const main = async () => {
