e0a07cc93e7202c0a13d33a8f94643a0eb66805b
Abdullah M. Bakry <abdaullah62@gmail.com>
2023-09-09 16:15:20 +0300

    fix: update the content generation

--- .bin/content.mjs
+++ .bin/content.mjs
@@ -33,7 +33,10 @@ const response = await notion.databases.query({
 })
 
 const firstPage = response.results[0]
-
+if (!firstPage) {
+    console.log('No blog post to write.')
+    process.exit()
+}
 const title = firstPage.properties.Name.title[0].plain_text
 const prompt = firstPage.properties.Prompt.rich_text?.[0]?.plain_text
 
@@ -52,37 +55,32 @@ const chatResponse = await openai.chat.completions.create({
     temperature: 0,
     max_tokens: 100,
 })
-// const gptResponse = await openai.createChatCompletion({
-//     model: 'gpt-3.5-turbo',
-//     messages: [
-//         {
-//             role: ChatCompletionRequestMessageRoleEnum.System,
-//             content: `You are a professional copywriter. You write posts for an internet software company. You write cleanly and concisely. Never start a blog post with a title. ONLY RESPOND IN MARKDOWN with just the content.`,
-//         },
-//         {
-//             role: ChatCompletionRequestMessageRoleEnum.User,
-//             content: `Write a blog post about: ${title}. ${prompt ?? ''}`,
-//         },
-//     ],
-//     temperature: 0.9,
-//     maxTokens: 100,
-// })
-
-console.log('Content: ', chatResponse.choices[0].message.content)
 
 const content = chatResponse.choices[0].message.content // gptResponse.data.choices[0].text
 const slug = slugify(title, { lower: true, strict: true }) // slugify the title meaning remove spaces and special characters and add dashes
 const date = new Date().toISOString().split('T')[0] // get the current date in the format YYYY-MM-DD
 
-await writeFile(path.join('blog', `${slug}.mdx`), content, 'utf8') // write the file to the blog folder
+const meta = `export const meta = {
+    title: '${title}',
+    date: '${date}',
+    excerpt: '',
+    image: 'api/og?title=${title}&author=Abdullah Bakry',
+    author: {
+        name: 'Abdullah Bakry',
+        image: '/me.png',
+    },
+}`
+
+const data = `${meta}\n\n${content}` // combine the meta data and the content
+
+await writeFile(path.join('blog', `${slug}.mdx`), data, 'utf8') // write the file to the blog folder
 
 await cd('blog') // change directory to the blog folder
 await $`git config user.name Abdullah M. Bakry"` // set the git user name
 await $`git config user.email "abdaullah62@gmail.com" ` // set the git user email
 await $`git checkout -b create-blog-post-${date}` // create a new branch with the current date
 await $`git add ${slug}.mdx` // add the file to git
-await $`git commit -m "Add blog post: ${title}"` // commit the file
+const commit = `Add blog post: ${title}` // create a commit message
+await $`git commit -m ${commit}` // commit the file
 await $`git push origin -u create-blog-post-${date}` // push the branch to github
-await $`gh pr create --title "Add blog post: ${title}" --fill` // create a pull request
-
-//3:13:00
+await $`gh pr create --fill` // create a pull request
