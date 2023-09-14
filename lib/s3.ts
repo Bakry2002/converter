5454431590148bfbdbc72078708807e81a741c75
Abdullah M. Bakry <abdaullah62@gmail.com>
2023-09-14 14:58:11 +0300

    feat: create s3 client file

---
+++ lib/s3.ts
@@ -0,0 +1,14 @@
+import { Artifact, Conversion } from '@prisma/client'
+import { S3 } from '@aws-sdk/client-s3'
+
+export const s3 = new S3({
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    },
+    region: process.env.AWS_DEFAULT_REGION,
+})
+
+// Key function: this function will generate a key for the file in s3
+export const key = (c: Conversion, n: number, a: Artifact) =>
+    `/${c.id}/${n}/${a.id}`
