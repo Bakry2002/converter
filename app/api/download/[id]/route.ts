2c68b117c6f4961958e23fbb65f151b664ccd8d4
Abdullah M. Bakry <abdaullah62@gmail.com>
2023-09-14 15:43:11 +0300

    fix: update the files to match the new data model

--- app/api/download/[id]/route.ts
+++ app/api/download/[id]/route.ts
@@ -1,18 +1,9 @@
-//* ========== Note ==============
-
-//* ========== End Note ===========
-import * as AWS from 'aws-sdk'
 import { prisma } from '@/lib/prisma'
 import { NextRequest, NextResponse } from 'next/server'
 import { Readable } from 'stream'
 import { ConversionStatus } from '@prisma/client'
-import { contentType, extension } from 'mime-types'
-
-AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_DEFAULT_REGION,
-})
+import { extension } from 'mime-types'
+import { key, s3 } from '@/lib/s3'
 const bucket = process.env.AWS_S3_BUCKET_NAME!
 
 type RouteParams = {
@@ -22,43 +13,49 @@ type RouteParams = {
 }
 
 export async function GET(req: NextRequest, { params }: RouteParams) {
-    //! find the conversion status by id
-    const conversions = await prisma.conversion.findUnique({
+    // find the conversion status by id
+    const conversion = await prisma.conversion.findUnique({
         where: {
             id: params.id,
+        },
+        include: {
+            stages: {
+                include: {
+                    artifacts: true,
+                },
+            },
         },
     })
 
     // if there is no conversion with that id
-    if (!conversions) {
+    if (!conversion) {
         return NextResponse.json({ status: 'Not found' }, { status: 404 })
     }
 
     // check if conversion is done
-    if (conversions.status !== ConversionStatus.DONE) {
+    if (conversion.status !== ConversionStatus.DONE) {
         return NextResponse.json(
             { status: 'Not done Converting yet!' },
             { status: 400 }
         )
     }
 
-    const s3 = new AWS.S3() // create new s3 client
-
     // params we can pass in to the upload function
     const downloadParams = {
         Bucket: bucket,
-        Key: conversions.s3Key,
+        Key: key(conversion, 1, conversion.stages[1].artifacts[0]), // get the key of the first artifact of the second stage i.e. the last stage
     }
 
     // const downloadResponse = await s3.getObject(downloadParams).promise() // get the file from S3 to be able to download it
-    const stream = Readable.toWeb(
-        s3.getObject(downloadParams).createReadStream()
-    ) // get the file from S3 to be able to download it, but with the Readable.toWeb() function, we can stream the file to the client instead of downloading it first
+    const stream = (
+        await s3.getObject(downloadParams)
+    ).Body?.transformToWebStream() // get the file from S3 to be able to download it, but with the Readable.toWeb() function, we can stream the file to the client instead of downloading it first
+
     return new NextResponse(stream as any, {
         headers: {
-            'Content-Type': conversions.toMime,
+            'Content-Type': conversion.stages[1].mime,
             'Content-Disposition': `attachment; filename=download.${extension(
-                conversions.toMime
+                conversion.stages[1].mime
             )}`,
         },
     })
