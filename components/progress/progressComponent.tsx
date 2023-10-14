aff8969965831ae77663a3aa5003e3bf58da7e1b
Abdullah M. Bakry <abdaullah62@gmail.com>
2023-10-14 17:44:21 +0300

    fix

--- components/progress/progressComponent.tsx
+++ components/progress/progressComponent.tsx
@@ -9,7 +9,11 @@ const progressComponent: React.FC<progressComponentProps> = ({}) => {
             {/* Progress box */}
             <div className="mx-auto p-8 bg-white/40 w-full">
                 {/* progress bar */}
-                <div className="absolute left-[calc()]"></div>
+                <div className="absolute left-[calc(16%+(44px/2))] w-[calc(66%+(60px/2))] top-[50px] text-center mx-auto bg-slate-500 h-[10px] mb-[5rem] rounded">
+                    {/* progress bar completion */}
+
+                    <div></div>
+                </div>
                 {/* Convert steps  */}
             </div>
         </div>
