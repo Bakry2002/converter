792cba5661ee1fe6789ae70f49f533117b50ab0b
Abdullah M. Bakry <abdaullah62@gmail.com>
2023-10-14 17:39:59 +0300

    fix

--- components/progress/progressComponent.tsx
+++ components/progress/progressComponent.tsx
@@ -3,7 +3,17 @@
 type progressComponentProps = {}
 
 const progressComponent: React.FC<progressComponentProps> = ({}) => {
-    return <div>progressComponent</div>
+    return (
+        // progress container
+        <div className="max-w-[950px] w-full h-full flex justify-center">
+            {/* Progress box */}
+            <div className="mx-auto p-8 bg-white/40 w-full">
+                {/* progress bar */}
+                <div className="absolute left-[calc()]"></div>
+                {/* Convert steps  */}
+            </div>
+        </div>
+    )
 }
 
 export default progressComponent
