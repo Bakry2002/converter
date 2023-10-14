cdc38b5f4cbf218d390d70d08359d7c7d2397003
Abdullah M. Bakry <abdaullah62@gmail.com>
2023-10-14 17:03:51 +0300

    add progress component

---
+++ components/progress/progressComponent.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+type progressComponentProps = {}
+
+const progressComponent: React.FC<progressComponentProps> = ({}) => {
+    return <div>progressComponent</div>
+}
+
+export default progressComponent
