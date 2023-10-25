f7970d896ab71dfce31a9db86682d9b9766d8be4
Abdullah M. Bakry <abdaullah62@gmail.com>
2023-10-26 00:53:03 +0300

    fix: some style on the home page

--- app/(home)/_components/SectionHeading.tsx
+++ app/(home)/_components/SectionHeading.tsx
@@ -18,7 +18,7 @@ const SectionHeading: React.FC<SectionHeadingProps> = ({ title }) => {
             )}
         >
             <h1
-                className="text-4xl font-bold relative text-center leading-10 pb-11 uppercase tracking-[2px] text-[#111]
+                className="text-2xl font-bold relative text-center leading-10 pb-11 uppercase tracking-[2px] text-[#111]
                 before:absolute before:bottom-5 before:w-full before:left-1/2 before:h-[1px] before:-ml-[50%] before:bg-[#777] before:z-[4]
                 after:absolute after:w-10 after:h-10 after:left-1/2 after:-translate-x-1/2 after:-ml-5 after:bottom-0 after:bg-[url('icon.png')] after:bg-no-repeat after:bg-contain after:bg-center after:text-3xl after:leading-10 after:text-[#c50000] after:font-normal after:z-[5] after:block after:bg-neutral-50   
             "
--- app/(home)/_components/Usage.tsx
+++ app/(home)/_components/Usage.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import SectionHeading from './SectionHeading'
+
 interface UsageProps {}
 
 const Usage: React.FC<UsageProps> = ({}) => {
@@ -25,37 +27,37 @@ const Usage: React.FC<UsageProps> = ({}) => {
         },
     ]
     return (
-        <section className="mx-28 mt-20">
-            <div className="mb-20">
-                <h1 className="text-3xl font-medium">
-                    How to use our Services
-                </h1>
-            </div>
+        <section className="bg-neutral-50 p-10">
+            <div className="mx-28 mt-10 flex flex-col justify-center gap-8">
+                <SectionHeading title="How to use Our services" />
 
-            {/* Box steps */}
-            <div className="grid grid-cols-1 xl:grid-cols-3">
-                {/* Step item */}
-                {steps.map((step) => (
-                    <div
-                        key={step.step}
-                        className="pl-[100px] xl:pl-[130px] pr-4 pb-6 xl:pb-0 relative min-h-[1px] mb-12 xl:m-0
+                {/* Box steps */}
+                <div className="grid grid-cols-1 xl:grid-cols-3">
+                    {/* Step item */}
+                    {steps.map((step) => (
+                        <div
+                            key={step.step}
+                            className="pl-[100px] xl:pl-[130px] pr-4 pb-6 xl:pb-0 relative min-h-[1px] mb-12 xl:m-0
                         before:absolute before:w-full xl:before:w-[1px] before:right-0 before:top-auto before:bottom-0 before:-mt-[38px] before:h-[1px] xl:before:h-full before:bg-[#85858580]
                         after:opacity-100 after:right-1/2 xl:after:right-[-33px] after:w-[18px] after:h-[18px] after:top-auto after:-bottom-[9px] xl:after:bottom-auto xl:after:top-1/2 xl:after:-translate-x-1/2 xl:after:-translate-y-1/2 after:mr-0 xl:after:mr-[15px] after:bg-white after:border-b-1 xl:after:border-b-0 xl:after:border-t-1 after:border-r-1 after:absolute after:rotate-45 after:-ml-[21px] after:transition-all after:duration-150 after:border-[#85858580]
                         last-of-type:before:hidden last-of-type:after:hidden
                     "
-                    >
-                        <div className="xl:flex xl:justify-center xl:items-baseline flex-col">
-                            {/* Step number */}
-                            <span className="shadow-step opacity-100 w-[70px] h-[70px] bg-white text-primary flex items-center justify-center rounded-full text-center text-4xl font-bold absolute left-0 xl:left-[42px]">
-                                {step.step}
-                            </span>
-                            <h3 className="mt-1 mb-5 text-2xl font-bold text-neutral-800">
-                                {step.title}
-                            </h3>
+                        >
+                            <div className="xl:flex xl:justify-center xl:items-baseline flex-col">
+                                {/* Step number */}
+                                <span className="shadow-step opacity-100 w-[70px] h-[70px] bg-white text-primary flex items-center justify-center rounded-full text-center text-4xl font-bold absolute left-0 xl:left-[42px]">
+                                    {step.step}
+                                </span>
+                                <h3 className="mt-1 mb-5 text-2xl font-bold text-neutral-800">
+                                    {step.title}
+                                </h3>
+                            </div>
+                            <p className="font-light text-lg">
+                                {step.description}
+                            </p>
                         </div>
-                        <p className="font-light text-lg">{step.description}</p>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
         </section>
     )
